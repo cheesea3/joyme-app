3c0b98a3f97c84d7ba550d3e93ae7e57513c953f
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-07-06 18:50:26 -0400

    forced login by assigning dummy user modal and removing validation
    accessed

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -7,6 +7,7 @@ import {AuthService} from '../../services/auth/auth.service';
 import firebase from 'firebase';
 import {UserService} from '../../services/user/user.service';
 import {PagePage} from '../page/page.page';
+import { forcedUserData } from 'src/app/models/user.model';
 
 @Component({
     selector: 'app-landing',
@@ -84,7 +85,7 @@ export class LandingPage implements OnInit {
 
     createPhoneForm() {
         this.phoneForm = this.fb.group({
-            phone: ['+972']
+            phone: ['+1']
         })
     }
 
@@ -95,17 +96,16 @@ export class LandingPage implements OnInit {
     }
 
     logIn(phoneForm: FormGroup) {
+        console.log('yo)');
         const appVerifier = this.windowRef.recaptchaVerifier;
-        const phoneNumber = '+972' + phoneForm.value.phone;
+        const phoneNumber = '+1' + phoneForm.value.phone;
         this.loadingSpinner = true;
         this.authService.loginPhone(phoneNumber, appVerifier).then(res => {
-            this.windowRef.confirmationResult = res;
+            this.windowRef.confirmationResult = '12345';
             this.showCodeFormStatus = 2;
         }).catch(err => {
-            if (err.code === 'auth/invalid-phone-number') {
-                this.errorMessage = 'מספר טלפון לא תקין';
-                this.presentToast(this.errorMessage);
-            }
+            this.windowRef.confirmationResult = '12345';
+            this.showCodeFormStatus = 2;
         }).finally(() => {
             this.loadingSpinner = false;
         })
@@ -113,10 +113,12 @@ export class LandingPage implements OnInit {
 
 
     verifyCode(codeForm: FormGroup) {
+        console.log('initial');
+console.log(codeForm.value.code);
         let code: string = codeForm.value.code.toString().trim();
-
+console.log(code);
         if (typeof code === 'string') {
-
+console.log('code is string');
             code = codeForm.value.code.toString().trim();
 
             if (!code) {
@@ -127,43 +129,26 @@ export class LandingPage implements OnInit {
 
             this.loadingSpinner = true;
 
-            this.windowRef.confirmationResult.confirm(code).then(res => {
-                this.userService.getById(res.user.uid).subscribe((user: any) => {
+                console.log('initial');
                     this.loadingSpinner = false;
-                    if (user) {
-                        if (user.status === 3) {
+                    if (forcedUserData) {
+                        console.log('pastuser');
+                        if (forcedUserData.status === 3) {
                             this.presentToast('החשבון שלך נחסם. למידע נוסף שלח לנו הודעה בכתובת contact@joyme.co.il', 8000);
                             this.showCodeFormStatus = 0;
                         } else {
-                            this.userService.setUser(user);
-                            this.authService.login(user);
+                            this.userService.setUser(forcedUserData);
+                            this.authService.login(forcedUserData);
                             this.router.navigate(['tabs/highlights']);
                         }
 
                     } else {
-                        this.router.navigate(['register-steps']).then(r => {
-                            this.userService.setUser({socialAuthId: res.user.uid});
-                        });
+                        this.userService.setUser(forcedUserData);
+                        this.authService.login(forcedUserData);
+                        this.router.navigate(['tabs/highlights']);
                     }
-                });
-            }).catch(err => {
-                this.loadingSpinner = false;
-
-                if (err.code === 'auth/argument-error' || err.code === 'auth/invalid-verification-code') {
-                    this.errorMessage = 'קוד לא תקין';
-                    this.presentToast(this.errorMessage);
-                }
-
-                if (err.code === 'auth/code-expired') {
-                    this.errorMessage = 'הקוד שלך כבר פג, נסה שוב';
-                    this.presentToast(this.errorMessage);
-                }
-            });
-        } else {
-            this.errorMessage = 'קוד לא תקין';
-            this.presentToast(this.errorMessage);
-        }
-    }
+         }
+}
 
     ionViewDidEnter() {
         setTimeout(_ => {
